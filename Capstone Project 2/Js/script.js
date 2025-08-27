cb7ad6d2d5335504092036cab5e9c5c021370ac6
Henrique Pereira <ricky1305prr@gmail.com>
2025-08-26 22:48:01 -0300

    add JavaScript

---
+++ Capstone Project 2/Js/script.js
@@ -0,0 +1,6 @@
+// ===== DARK MODE TOGGLE =====
+const toggleBtn = document.getElementById("darkModeToggle");
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    toggleBtn.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
